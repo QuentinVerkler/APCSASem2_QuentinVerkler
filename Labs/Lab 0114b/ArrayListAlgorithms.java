a458669e26d4f46b01506fc0ee6068f93d6740ea
QuentinVerkler <43121166+QuentinVerkler@users.noreply.github.com>
2019-01-14 10:31:01 -0800

    asdf

--- Labs/Lab 0114b/ArrayListAlgorithms.java
+++ Labs/Lab 0114b/ArrayListAlgorithms.java
@@ -1,16 +1,23 @@
 import java.util.ArrayList;
 /**
- * Write a description of class Algorithem here.
+ * Write a description of class ArrayListAlgorithms here.
  *
  * @author (your name)
  * @version (a version number or a date)
  */
-public class Algorithem
+public class ArrayListAlgorithms
 {
+    //variables
+    public static int compare = 0;
+    public static int swap = 0;
+    public static long time = 0;
     public static void main(){
-        int[] nums = new int[20];
-        for(int i = 0; i < nums.length; i++){
-            nums[i] = (int)(Math.random() * 10);
+        compare = 0;
+        swap = 0;
+        time = 0;
+        ArrayList<Integer> random = new ArrayList<Integer>();
+        for(int i = 0; i < 20; i++){
+            random.add((int)(Math.random() * 10));
         }
         
         ArrayList<Integer> sorted = mySort(nums);
@@ -20,21 +27,30 @@ public class Algorithem
         }
         System.out.println();
         
+        /*
         for(int i = 0; i < sorted.size(); i++){
             System.out.print(sorted.get(i) + ", ");
         }
-        System.out.println(); 
+        System.out.println(); */
+        
         
         //bubbleSort(nums);
         //selectionSort(nums);
-        /*for(int i = 0; i < nums.length; i++){
+        insertSort(nums);
+        
+        for(int i = 0; i < nums.length; i++){
             System.out.print(nums[i] + ", ");
         }
-        System.out.println(); */
+        System.out.println();
+        System.out.println("Compare: " + compare);
+        System.out.println("Swap: " + swap);
+        System.out.println("Time: " + time);
+        
     }
     
     //fix this
     public static ArrayList<Integer> mySort(int[] nums){
+        long tStart = System.nanoTime();
         ArrayList<Integer> sorted = new ArrayList<Integer>();
         int smallest = 10000;
         //finding smallest number
@@ -49,22 +65,32 @@ public class Algorithem
             for(int i = 0; i < nums.length; i++){
                 if(nums[i] == smallest){
                     sorted.add(nums[i]);
+                    swap++;
                 }
+                compare++;
             }
             smallest++;
         }
+        long tEnd = System.nanoTime();
+        time = tEnd - tStart;
         return sorted;
     }
     
     public static void bubbleSort(int[] nums){
+        long tStart = System.nanoTime();
         int end = nums.length - 1; 
+        //outside loop starts and end
         while(end > 1){
+            //inside loop starts at beginning and ends at end
             for(int i = 0; i < end; i++){
+                //actual swappers
                 if(nums[i] > nums[i+1]){
                     int temp = nums[i];
                     nums[i] = nums[i+1];
                     nums[i+1] = temp;
+                    swap++;
                 }
+                compare++;
             }
             if(nums[end] > nums[end-1]){
                 end -= 1;
@@ -73,19 +99,49 @@ public class Algorithem
                 end--;
             }
         }
+        long tEnd = System.nanoTime();
+        time = tEnd - tStart;
     }
     
     public static void selectionSort(int[] nums){
+        long tStart = System.nanoTime();
+        //first loop starts at beginning
         for(int i = 0; i < nums.length - 1; i++){
             int place = i;
+            //second loop starts at one above i
             for(int j = i + 1; j < nums.length; j++){
+                //swapper
                 if(nums[j] < nums[place]){
                     place = j;
                 }
+                compare++;
             }
             int smallNum = nums[place];
             nums[place] = nums[i];
             nums[i] = smallNum;
+            swap++;
         }
+        long tEnd = System.nanoTime();
+        time = tEnd - tStart;
+    }
+    
+    public static void insertSort(int[] nums){
+        long tStart = System.nanoTime();
+        //starts at beginning
+        for(int i = 1; i < nums.length; i++){
+            //starts at i
+            for(int j = i; j > 0; j--){
+                //Swapping code
+                if(nums[j] < nums[j-1]){
+                    int temp = nums[j];
+                    nums[j] = nums[j-1];
+                    nums[j-1] = temp;
+                    swap++;
+                }
+                compare++;
+            }
+        }
+        long tEnd = System.nanoTime();
+        time = tEnd - tStart;
     }
 }
