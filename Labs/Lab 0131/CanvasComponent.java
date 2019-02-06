1b5c7a2afb3c488cf7d114274bab9e0c235de905
QuentinVerkler <43121166+QuentinVerkler@users.noreply.github.com>
2019-02-06 10:16:52 -0800

    asdf

--- Labs/Lab 0131/CanvasComponent.java
+++ Labs/Lab 0131/CanvasComponent.java
@@ -7,7 +7,8 @@ import java.awt.event.*;
  * @author (your name)
  * @version (a version number or a date)
  */
-public class CanvasComponent extends JComponent implements MouseListener, MouseMotionListener
+public class CanvasComponent extends JComponent 
+        implements MouseListener, MouseMotionListener, ActionListener, KeyListener
 {
     private int x = 0;
     private int y = 0;
@@ -16,6 +17,11 @@ public class CanvasComponent extends JComponent implements MouseListener, MouseM
     private int mouseFromX = 0;
     private int mouseFromY = 0;
     private boolean shapeSelected;
+    private int animationDeltaX = 1;
+    private int animationDeltaY = 0;
+    private int gutterX = 10;
+    private int gutterY = 10;
+    private Timer animationTimer;
     //convas constructor
     CanvasComponent(int tempWidth, int tempHeight){
         setSize(tempWidth, tempHeight);
@@ -25,14 +31,18 @@ public class CanvasComponent extends JComponent implements MouseListener, MouseM
         y = tempHeight;
         this.addMouseListener(this);
         this.addMouseMotionListener(this);
+        animationTimer = new Timer(20, this);
+        animationTimer.start();
     }
     
     //all methods
+    //paint methods
     public void paintComponent(Graphics g){
         g.setColor(Color.red);
         g.fillRect(x, y, width, height);
     }
     
+    //mouse methods
     public void mouseClicked(MouseEvent e){
         
     }
@@ -62,10 +72,49 @@ public class CanvasComponent extends JComponent implements MouseListener, MouseM
         int mouseToY = e.getY();
         x += mouseToX - mouseFromX;
         y += mouseToY - mouseFromY;
+        x = e.getX();
+        y = e.getY();
         repaint();
     }
     
     public void mouseMoved(MouseEvent e){
         
     }
+    
+    //ActionListener (animation) method
+    public void actionPerformed(ActionEvent e){
+        Dimension componentSizeDimension = this.getSize();
+        if(x + width + gutterX > componentSizeDimension.getWidth()){
+            animationDeltaX = 0;
+            animationDeltaY = 1;
+            x = (int)(componentSizeDimension.getWidth()) - width - gutterX - 1;
+            y += animationDeltaY;
+        }
+        if(y + gutterY + height > componentSizeDimension.getHeight()){
+            animationDeltaX = -1;
+            animationDeltaY = 0;
+            y = (int)(componentSizeDimension.getHeight()) - height - gutterY;
+            x += animationDeltaX;
+        }
+        if(x < gutterX){
+            animationDeltaX = 0;
+            animationDeltaY = -1;
+            x = gutterX;
+            y += animationDeltaY;
+        }
+        if(y < gutterY){
+            animationDeltaX = 1;
+            animationDeltaY = 0;
+            y = gutterY;
+            x += animationDeltaX;
+        }
+        else{
+            x += animationDeltaX;
+            y += animationDeltaY;
+        }
+        repaint();
+    }
+    
+    //KeyListener methods
+    
 }
